16284111a44dc16444523539561fba0a5e7c361f
Made Baruna <made.setia@gmail.com>
2019-01-28 23:25:54 +0700

    Add not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import { Container } from 'semantic-ui-react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { GlobalStyle } from './GlobalStyle';
+import { client } from './graphql/client';
 import Header from './components/Header';
 import UserList from './pages/UserList';
-import { client } from './graphql/client';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   public render() {
@@ -19,6 +20,7 @@ class App extends Component {
               <Header />
               <Switch>
                 <Route exact path="/" component={UserList} />
+                <Route component={NotFoundPage} />
               </Switch>
             </>
           </Router>
